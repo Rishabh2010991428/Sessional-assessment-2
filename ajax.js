0a091ba88028bafff06aa112b3c8af1a69654c0c
Rishabh <rishabh1428.be20@chitkara.edu.in>
2022-08-11 16:44:17 +0530

    added ajax file and code for api integration

---
+++ ajax.js
@@ -0,0 +1,45 @@
+
+let popBtn = document.getElementById("printdetail");
+
+// adding event listener
+printdetail.addEventListener('click', popbtnClickHandler);
+
+// adding function for event
+function popbtnClickHandler(){
+    console.log("printdetail button clicked!!");
+
+    // Instantiate an XHR object
+    const xhr = new XMLHttpRequest();
+
+    // opening object
+    // for get request
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09', true);
+
+    xhr.onload = function(){
+        if(this.status===200){
+            let obj = JSON.parse(this.responseText);
+            console.log(obj);
+            let list = document.getElementById('list');
+            str = "";
+            for (key in obj)
+            {
+                str += `<li>
+                Name: ${obj[key].name} 
+                Username: ${obj[key].username}
+                email: ${obj[key].email}
+    
+                
+                </li>`;
+            }
+            list.innerHTML = str;
+
+        }
+        else{
+            console.log("Some error occured!!");
+        }
+    }
+
+    // sending request
+    xhr.send();
+    console.log("We are done fetching person details!");
+}
